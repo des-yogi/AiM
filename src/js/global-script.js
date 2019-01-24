b405485fefa9e5d709d70eda2ac6aa3b1798782a
des-yogi <nuevo-design@mail.ru>
2019-01-24 19:19:49 +0200

    #in progress: product-page

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -22,6 +22,35 @@
 // });
 
 (function(){
-  // code
-
+  var productSwiper = new Swiper('.product__col-slider', {
+    speed: 400,
+    spaceBetween: 20,
+    // grabCursor: true,
+    effect: 'fade',
+    /*zoom: true,
+    zoom: {
+      maxRatio: 2,
+    },*/
+    /*autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },*/
+    hashNavigation: {
+      watchState: true,
+      replaceState: true,
+    },
+    navigation: {
+      nextEl: '.product__button-next',
+      prevEl: '.product__button-prev',
+    },
+    pagination: {
+      el: '.product__pagination',
+      clickable: true,
+      //dynamicBullets: true,
+    },
+    a11y: {
+      prevSlideMessage: 'Предыдущий слайд',
+      nextSlideMessage: 'Следующий слайд',
+    },
+  });
 }());
